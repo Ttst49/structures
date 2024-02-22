92002f1592ae37596376f3827e60eb65a8fee350
Ttst <thibautstachnick@gmail.com>
2024-02-22 18:31:55 +0100

    change import

--- modules/employe.cpp
+++ modules/employe.cpp
@@ -1,15 +1,15 @@
-#include "modules/employe.h"
-#include "iostream"
+#include "../headers/employe.h"
+#include <iostream>
 using namespace std;
 
 [[maybe_unused]]
 void saisir_employe(employe &e){
-    cout << "Donnez le nom du nouvel employé"; cin >> e.nom;
-    cout << "Donnez le prénom du nouvel employé"; cin >> e.prenom;
-    cout << "Donnez le salaire du nouvel employé"; cin >> e.salaire;
+    cout << "Donnez le nom du nouvel employé" << endl; cin >> e.nom;
+    cout << "Donnez le prénom du nouvel employé" << endl; cin >> e.prenom;
+    cout << "Donnez le salaire du nouvel employé" << endl; cin >> e.salaire;
 }
 
 [[maybe_unused]]
 void afficher_employe(employe &e){
-    cout << e.nom << " " << e.prenom << "|" << e.salaire;
+    cout << e.nom << " " << e.prenom << "|" << e.salaire << endl;
 }
