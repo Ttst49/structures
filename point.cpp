7f2cd046c0f31ade9bd5591c07a7714914b21022
Ttst <thibautstachnick@gmail.com>
2024-02-22 16:51:03 +0100

    add methods from point.h

--- point.cpp
+++ point.cpp
@@ -4,7 +4,29 @@
 
 using namespace std;
 
+[[maybe_unused]]
 void saisir_point(point &p){
     cout << "Tapez l'abscisse du point : "; cin >> p.x;
     cout << "Tapez l'ordonnée du point : "; cin >> p.y;
+}
+
+[[maybe_unused]]
+void afficher_point(point p){
+    cout << "L'abscisse de point x :" << p.x;
+    cout << "L'abscisse de point y :" << p.y;
+}
+
+[[maybe_unused]]
+double distance(point a, point b){
+    double dx,dy;
+    dx = a.x - b.x;
+    dy = a.y - b.y;
+
+    return sqrt(dx*dx+dy*dy);
+}
+
+[[maybe_unused]]
+void milieu(point a, point b, point &m){
+    m.x = (a.x + b.x) /2;
+    m.y = (a.y + b.y) /2;
 }
